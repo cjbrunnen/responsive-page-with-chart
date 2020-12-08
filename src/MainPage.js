516d8f0815f8cee7e6fe9b93a501bad5af556ee0
Caz Brunnen <cazgotsoul@gmail.com>
2020-12-08 08:02:07 +0000

    fix: tidy up file structure

--- src/MainPage.js
+++ src/MainPage.js
@@ -11,7 +11,7 @@ const StyledContainer = styled(Container)`
   margin-top: 35px;
 `;
 const Sitename = styled(Typography)`
-  font-size: 26px;
+  font-size: 26px !important;
   margin: 9px 4px;
   display: inline-block;
   font-family: Arial, sans-serif;
@@ -24,8 +24,8 @@ const WebLink = styled(Sitename)`
 export default function MainPage() {
   return (
     <StyledContainer maxWidth="md">
-      <Sitename>Leicester City Council</Sitename>
-      <WebLink>https://www.leicester.gov.uk</WebLink>
+      <Sitename>Leicester City Council</Sitename>{' '}
+      <WebLink> https://www.leicester.gov.uk</WebLink>
       <Cta />
       <LineChart />
       <EnhancedTable />
